4c00d41a9c9c134813ce65940ce1621d0fbfc3ca
Pablo <pablomatemat@gmail.com>
2022-06-15 10:09:21 -0300

    modificado os bonus

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvieira- <pvieira-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 17:14:25 by pvieira-          #+#    #+#             */
+/*   Updated: 2022/06/14 17:20:04 by pvieira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	temp;
+	
+	temp = *lst;
+	while (temp->next != NULL)
+		temp = temp->next;
+	new->next = NULL;
+	temp_next = new;
+}
---
+++ libft/ft_lstlast_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvieira- <pvieira-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 16:58:59 by pvieira-          #+#    #+#             */
+/*   Updated: 2022/06/14 22:28:25 by pvieira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	temp;
+
+	temp = lst;
+	while (temp->next != NULL)
+		temp = temp->next;
+	return(temp);
+}
